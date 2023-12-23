f7eb032696c88bb5afb8a08a62c948cd467a5a33
Noman-Nom <noman5456318@gmail.com>
2023-12-23 18:59:29 +0500

    histogram equilization completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
   
     <>
-    <div>App</div>
+  
     <BrowserRouter>
     <Routes>
       
--- src/Components/Histogram/Histogram.jsx
+++ src/Components/Histogram/Histogram.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import './Histogram.scss'
 import { useState } from 'react'
 
 const Histogram = () => {
 
   const [inputData, setinputData] = useState(Array(8).fill(''))
-  
 
-  const handleChange =  (index,value)=>{
+  const [v1, setV1] = useState('')
+  const [v2, setV2] = useState('')
+
+  const [size, setSize] = useState(0)
+
+ 
+
+  console.log(typeof v1)
+  console.log(typeof v2)
+
+
+
+
+
+  const handleSize = () => {
+
+    const sizeOfImage = parseInt(v1) * parseInt(v2)
+
+    setSize(sizeOfImage)
+  }
+
+  console.log("size = ", size)
+
+
+  const handleChange = (index, value) => {
+
+
 
     const newInputData = [...inputData]
 
@@ -16,30 +41,36 @@ const Histogram = () => {
     newInputData[index] = value
     setinputData(newInputData)
 
-    // console.log("values= ", value) tageting input with index
+    // console.log("values= ", value)
+    // tageting input with index
 
   }
 
-  const calculate = ()=>{
 
-    const normalizeData = inputData.map((value)=> parseFloat(value)/4096)
+  const calculate = () => {
 
-    // console.log( "normalizeData = ", normalizeData)
-    const cdf = normalizeData.reduce((accumulator , currentValue, index)=>{
 
-      accumulator.push(index > 0 ?  accumulator[index -1 ] +currentValue: currentValue)
+
+
+    const normalizeData = inputData.map((value) => parseFloat(value) / size)
+
+    console.log("normalizeData = ", normalizeData)
+    const cdf = normalizeData.reduce((accumulator, currentValue, index) => {
+
+      accumulator.push(index > 0 ? accumulator[index - 1] + currentValue : currentValue)
 
 
       // console.log( "cdf=", accumulator)
       return accumulator
-    },[])
+    }, [])
 
-    // console.log("CDF = ", cdf)
+    console.log("CDF = ", cdf)
 
-    const equiliziedHistogram = cdf.map((value)=>(Math.round(value*7))
-    )
+    const equiliziedHistogram = cdf.map((value) => (Math.round(value * 7)))
 
-        console.log("EquilizedHisogram = ", equiliziedHistogram)
+
+            console.log("equi" , equiliziedHistogram) // this ans displayed in second time clicked why ?
+
 
 
   }
@@ -47,18 +78,25 @@ const Histogram = () => {
   return (
     <div className='container'>
 
+
+      <input type="text" value={v1} onChange={(e) => setV1(e.target.value)} />
+      <input type="text" value={v2} onChange={(e) => setV2(e.target.value)} />
+
+      <button onClick={handleSize}>Calculaate image Size </button>
+
+
       <table id='inputData'>
-        {inputData.map((value,index)=>(
-            <tr key={index}>
-              <td>
-                <input type="text"
+        {inputData.map((value, index) => (
+          <tr key={index}>
+            <td>
+              <input type="text"
 
-                  value={value}
-                  onChange={(e)=> handleChange(index , e.target.value)}
-                />
-              </td>
+                value={value}
+                onChange={(e) => handleChange(index, e.target.value)}
+              />
+            </td>
 
-            </tr>
+          </tr>
 
         ))}
       </table>
