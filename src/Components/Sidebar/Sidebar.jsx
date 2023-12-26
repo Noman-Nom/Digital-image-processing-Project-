971e8b540ad9e90d278bf0c709b32510f27271dc
Noman-Nom <noman5456318@gmail.com>
2023-12-27 02:54:31 +0500

    routing working completed

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,14 +1,5 @@
 import React, { useContext } from "react";
 import "./Sidebar.scss";
-import SpaceDashboardIcon from "@mui/icons-material/SpaceDashboard";
-import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
-import ManIcon from "@mui/icons-material/Man";
-import ImportContactsIcon from "@mui/icons-material/ImportContacts";
-import ClassIcon from "@mui/icons-material/Class";
-import AutoStoriesIcon from "@mui/icons-material/AutoStories";
-import TextFieldsIcon from "@mui/icons-material/TextFields";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import LockIcon from "@mui/icons-material/Lock";
 
 import { Link } from "react-router-dom";
 import { MdOutlineHideImage } from "react-icons/md";
@@ -23,7 +14,7 @@ import { IoLogoEdge } from "react-icons/io5";
 
 const Sidebar = () => {
 
- 
+
   return (
     <div className="sidebar">
       <div className="top">
@@ -38,87 +29,94 @@ const Sidebar = () => {
         <ul>
           <p className="title">Image Negative</p>
 
-          <Link to="/" style={{ textDecoration: "none" }}>
+          <Link to="/gscalenegative" style={{ textDecoration: "none" }}>
             <li>
-              <MdOutlineHideImage  className="icon" />
+              <MdOutlineHideImage className="icon" />
               <span>Gray-Scale Image (-)</span>
             </li>
+          </Link>
+          <Link to='/gscalenegative' style={{ textDecoration: "none" }}>
             <li>
-              <MdOutlineHideImage  className="icon" />
+              <MdOutlineHideImage className="icon" />
               <span>Color Image (-)</span>
             </li>
+          </Link>
+          <Link to='/negetive' style={{ textDecoration: "none" }}>
             <li>
-              <MdOutlineHideImage  className="icon" />
+              <MdOutlineHideImage className="icon" />
               <span>By Values (-)</span>
             </li>
           </Link>
 
+
           <p className="title">Intensity Transformation</p>
 
-          <Link to="/user" style={{ textDecoration: "none" }}>
+          <Link to="/thresholdAlg" style={{ textDecoration: "none" }}>
             <li>
               <BsImageFill className="icon" />
               <span>Thresholding</span>
             </li>
           </Link>
-          <Link to="/user" style={{ textDecoration: "none" }}>
+          <Link to="/threshold" style={{ textDecoration: "none" }}>
             <li>
               <BsImageFill className="icon" />
               <span>Thresholding (calc)</span>
             </li>
           </Link>
-          
-          <Link to="/user" style={{ textDecoration: "none" }}>
+
+          <Link to="/powerlaw" style={{ textDecoration: "none" }}>
             <li>
               <BsImageFill className="icon" />
               <span>PowerLaw Transformation</span>
             </li>
           </Link>
-          
-          <Link to="/user" style={{ textDecoration: "none" }}>
+
+          <Link to="/powerlaw" style={{ textDecoration: "none" }}>
             <li>
               <BsImageFill className="icon" />
               <span>PowerLaw Transformation (calc)</span>
             </li>
           </Link>
-          <Link to="/user" style={{ textDecoration: "none" }}>
+          <Link to="/colortogray" style={{ textDecoration: "none" }}>
             <li>
               <BsImageFill className="icon" />
               <span>Color To GrayScale</span>
             </li>
           </Link>
-          
+
           <p className="title">Image Enhancement</p>
-          <Link to="/courses" style={{ textDecoration: "none" }}>
+          <Link to="/histogram" style={{ textDecoration: "none" }}>
             <li>
               < PiImagesSquare className="icon" />
               <span>Histogram Equilization</span>
             </li>
           </Link>
-         
-       
-          <p className="title">OpenSource Libraries</p>
-          <li>
-            < FaFaceGrinStars  className="icon" />
 
-            <span>Face Detector</span>
-          </li>
-          <Link to="/courses" style={{ textDecoration: "none" }}>
+
+          <p className="title">OpenSource Libraries</p>
+          <Link to='/facedetector' style={{ textDecoration: "none" }} >
+            <li>
+              < FaFaceGrinStars className="icon" />
+
+              <span>Face Detector</span>
+            </li>
+          </Link>
+          <Link to="/imtt" style={{ textDecoration: "none" }}>
             <li>
               < CiText className="icon" />
               <span>Image To Text</span>
             </li>
           </Link>
-          <Link to="/courses" style={{ textDecoration: "none" }}>
+          <Link to="/edge" style={{ textDecoration: "none" }}>
             <li>
               <  IoLogoEdge className="icon" />
               <span>Edge Detector</span>
             </li>
           </Link>
-          
+
         </ul>
       </div>
-      
+
     </div>
   );
 };
