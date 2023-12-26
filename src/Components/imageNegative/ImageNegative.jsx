05279b9167b4cb45f97005e58f2b83442c114a9a
Noman-Nom <noman5456318@gmail.com>
2023-12-26 13:00:09 +0500

    image negetive algorithim completed

--- src/Components/imageNegative/ImageNegative.jsx
+++ src/Components/imageNegative/ImageNegative.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import './ImageNegative.scss'
 
 const ImageNegativeAlgorithm = () => {
     const [resultMatrix, setResultMatrix] = useState([]);
     const [inputValues, setInputValues] = useState(Array(4).fill(Array(4).fill(0)));
-    const [bitdepth, setBitdepth] = useState(4)
+    const [bitdepth, setBitdepth] = useState(0)
+    const [bitDepthValue, setBitDepthValue] = useState(null)
+    // console.log("bitdepth = ",  bitdepth)
 
-    console.log("bitdepth = ",  bitdepth)
+    const handleBd = ()=>{
 
+        const bd = 2**bitdepth - 1
+        setBitDepthValue(bd)
 
+            console.log("bd value = ",  bitdepth)
+            
+    }
+
+    useEffect(()=>{
+        console.log('Bit Depth Value:', bitDepthValue);
+        
+    },[bitDepthValue])
+           
     const handleInputChange = (rowIndex, colIndex, event) => {
         const { value } = event.target;
-        const newValue = isNaN(value) ? 0 : Math.max(0, Math.min(15, parseInt(value, 10)));
+        const newValue = isNaN(value) ? 0 : Math.max(0, Math.min(255, parseInt(value, 10)));
 
         setInputValues((prevInputValues) => {
             const newInputValues = [...prevInputValues];
@@ -23,7 +36,8 @@ const ImageNegativeAlgorithm = () => {
     };
 
     const calculate = () => {
-        const result = inputValues.map((row) => row.map((value) => 15 - value));
+        console.log("value = ", bitDepthValue)
+        const result = inputValues.map((row) => row.map((value) => bitDepthValue - value));
 
         setResultMatrix(result);
         displayMatrix('resultMatrix', result);
@@ -51,7 +65,7 @@ const ImageNegativeAlgorithm = () => {
             <h1><FaQuoteLeft />  Image Negative <span>Algorithim.. </span><FaQuoteRight /></h1>
                     <div className='bitdepth'><input  onChange={(e)=>setBitdepth(e.target.value)} type="text"  placeholder='Enter Bitdepth' />
                   <div className='calculate-btn'>
-                    <button>Calculate BD</button>
+                    <button onClick={handleBd}>Calculate BD</button>
 
                   </div>
                    
@@ -68,7 +82,7 @@ const ImageNegativeAlgorithm = () => {
                                             <input
                                                 value={value}
                                                 min="0"
-                                                
+                                               
                                                 type="text"
                                                 onChange={(e) => handleInputChange(rowIndex, colIndex, e)}
                                             />
